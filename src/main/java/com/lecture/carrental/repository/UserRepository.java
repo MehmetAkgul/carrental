d83719c811778fe26b75ee8762ac3396157ef1ae
MEHMET AKGÜL <79218941+MehmetAkgul@users.noreply.github.com>
2022-02-17 00:15:30 +0300

    jwt

--- src/main/java/com/lecture/carrental/repository/UserRepository.java
+++ src/main/java/com/lecture/carrental/repository/UserRepository.java
@@ -2,10 +2,18 @@ package com.lecture.carrental.repository;
 
 import com.lecture.carrental.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.util.Optional;
 
 @Repository
+@Transactional// içerideki tüm metodların aynı anda yürütülmesini istediğimizde bunu yazmamız lazım
 public interface UserRepository extends JpaRepository<User, Long> {
 
 
+   // @Query("SELECT u from User u where u.email=?1")  // bu otomatik oluşuyor  o yüzden yorum
+    Optional<User> findByEmail(String email);
 }
