0998e1b6d5b0b3fde5a6c43cd31752c454e95b01
MEHMET AKGÜL <79218941+MehmetAkgul@users.noreply.github.com>
2022-03-02 21:01:56 +0300

    rezervation query

--- src/main/java/com/lecture/carrental/controller/ReservationController.java
+++ src/main/java/com/lecture/carrental/controller/ReservationController.java
@@ -2,6 +2,7 @@ package com.lecture.carrental.controller;
 
 import com.lecture.carrental.domain.Car;
 import com.lecture.carrental.domain.Reservation;
+import com.lecture.carrental.dto.ReservationDTO;
 import com.lecture.carrental.service.ReservationService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,6 +15,7 @@ import javax.validation.Valid;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @AllArgsConstructor
@@ -23,6 +25,59 @@ import java.util.Map;
 public class ReservationController {
 
     public ReservationService reservationService;
+
+
+    @GetMapping("/admin/all")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<List<ReservationDTO>> getAllReservation() {
+        List<ReservationDTO> reservationDTOS = reservationService.fetchAllRezervations();
+        return new ResponseEntity<>(reservationDTOS, HttpStatus.OK);
+    }
+
+
+
+    @GetMapping("/admin/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<ReservationDTO> getReservationById(@PathVariable Long id) {
+        ReservationDTO reservationDTOS = reservationService.findById(id);
+        return new ResponseEntity<>(reservationDTOS, HttpStatus.OK);
+    }
+
+
+    @GetMapping("/admin/auth/all")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<List<ReservationDTO>> getAllUserReservations(@RequestParam(value = "userId") Long userId) {
+        List<ReservationDTO> reservations = reservationService.findAllByUserId(userId);
+        return new ResponseEntity<>(reservations, HttpStatus.OK);
+    }
+    @PostMapping("/add/auth")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Map<String, Boolean>> addReservation(@RequestParam (value = "userId") Long userId,
+                                                               @RequestParam (value = "carId") Car carId,
+                                                               @Valid @RequestBody Reservation reservation) {
+        reservationService.addReservation(reservation, userId, carId);
+        Map<String, Boolean> map = new HashMap<>();
+        map.put("Reservation added successfully!", true);
+        return new ResponseEntity<>(map, HttpStatus.CREATED);
+    }
+
+
+    @GetMapping("/{id}/auth")
+    @PreAuthorize("hasRole('CUSTOMER') or hasRole('ADMIN')")
+    public ResponseEntity<ReservationDTO> getUserReservationById(@PathVariable Long id, HttpServletRequest request) {
+        Long userId = (Long) request.getAttribute("id");
+
+        ReservationDTO reservation = reservationService.findByIdAndUserId(id, userId);
+        return new ResponseEntity<>(reservation, HttpStatus.OK);
+    }
+
+    @GetMapping("/auth/all")
+    @PreAuthorize("hasRole('CUSTOMER') or hasRole('ADMIN')")
+    public ResponseEntity<List<ReservationDTO>> getUserReservationsByUserId(HttpServletRequest request) {
+        Long userId = (Long) request.getAttribute("id");
+        List<ReservationDTO> reservations = reservationService.findAllByUserId(userId);
+        return new ResponseEntity<>(reservations, HttpStatus.OK);
+    }
 
     @PostMapping("/add")
     @PreAuthorize("hasRole('CUSTOMER') or hasRole('ADMIN')")
--- src/main/java/com/lecture/carrental/repository/ReservationRepository.java
+++ src/main/java/com/lecture/carrental/repository/ReservationRepository.java
@@ -1,7 +1,10 @@
 package com.lecture.carrental.repository;
 
 import com.lecture.carrental.domain.Reservation;
+import com.lecture.carrental.domain.User;
 import com.lecture.carrental.domain.enumeration.ReservationStatus;
+import com.lecture.carrental.dto.ReservationDTO;
+import com.lecture.carrental.exception.ResourceNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -9,10 +12,17 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 @Transactional
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    //    @Query("SELECT new com.lecture.carrental.dto.ReservationDTO(r) FROM Reservation r WHERE r.userId = ?1")
+    List<ReservationDTO> findAllByUserId(User user);
+
+    //    @Query("SELECT new com.lecture.carrental.dto.ReservationDTO(r) FROM Reservation r WHERE r.id = ?1 and r.userId = ?2")
+    Optional<ReservationDTO> findByIdAndUserId(Long id, User user) throws ResourceNotFoundException;
 
     @Query("SELECT r FROM Reservation r " +
             "LEFT JOIN fetch r.carId cd " +
@@ -24,6 +34,10 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
             "?3 BETWEEN r.pickUpTime and r.dropOffTime)")
     List<Reservation> checkStatus(Long carId, LocalDateTime pickUpTime, LocalDateTime dropOffTime,
                                   ReservationStatus done, ReservationStatus canceled);
+
+    List<ReservationDTO> findAllBy();
+
+    ReservationDTO findByIdOrderById(Long id) throws  ResourceNotFoundException;
 }
 
 
